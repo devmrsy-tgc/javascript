3f7476c80833ecd1c9379024db69b63b8d83518f
sooraj tgc <sooraj.tgc@gmail.com>
2024-02-11 10:45:44 +0530

    all about till 4th session

--- day1/index.js
+++ day1/index.js
@@ -1,6 +1,4 @@
-// let hosted = "hoised";
-
-// console.log(hosted)
-
-
-
+// html -> it is not a programming lang. it is markup lang.
+// css -> cascading style sheet...
+// bootstrap -> css framework
+// javascript is a programming language
