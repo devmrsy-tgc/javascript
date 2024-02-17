794e7037d0a15abb9aad87ae40acae88d607d735
sooraj <franksooraj@gmail.com>
2024-02-17 11:55:57 +0530

    operator, arithmetic, assignment

---
+++ day3/index.js
@@ -0,0 +1,63 @@
+// create a variable and assign a string as 'hello' and print it. Use only es6 syntax.
+
+// let sAsLet = 'sAsLet';
+// const sAsConst = 'sAsConst';
+
+// console.log(sAsLet);
+// console.log(sAsConst);
+
+// operator in JS -> +, -, *, /, %, **
+
+// let a = 10;
+// let b = 20;
+// let c = 4;
+// let d = 2;
+
+// BODMAS
+// result = a + b - c / d;
+// a + b - 2;
+// 30 - 2;
+// 28
+// result = a + (b - c) / d; // k-18, h-18, g-18, s-18, a-18 
+// result = a + 16 / d;
+// result = a + 8;
+// result = 18;
+
+// + 
+
+// let varAsStr = 'opps!'; // gw,a->oops30, g->oops30,
+// console.log(`varAsStr:${typeof varAsStr} || a:${typeof a}`)
+
+// console.log( (varAsStr+a + 12))
+
+// result = varAsStr  a + b; // 30oops!, k-> invalid, h-> invalid, g -> invalid
+
+// let numAsNumber = 30;
+// let numAsString = '30';
+
+// console.log(resul/t)
+
+
+// let varAsStr = 'opps!';
+
+// let a = 10;
+// let b = 10;
+
+// console.log(varAsStr * (a+b));
+
+// let var1 = 10;
+
+// let var2 = '10';
+
+// if(var1 !== var2) { // '==' comparison operator
+//     console.log('var1 and var2 have equal potential!!')
+// } else {
+//     console.log('var1 and var2 have different potential!!')
+// }
+
+//  increment (++)
+
+let num = 10;
+num-- 
+
+console.log(num)
