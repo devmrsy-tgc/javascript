608ccab0cd7787c956fa6030248d22ff4f02a507
sooraj <franksooraj@gmail.com>
2024-03-02 10:43:48 +0530

    day_6 doubt | rest operator

--- day6/index.js
+++ day6/index.js
@@ -21,20 +21,54 @@
 
 // console.log('result1', result1);
 // console.log('result2', result2);
-const window = {key:'value'}
+// const window = { key: 'value' };
 
-const userDetail = {
-	firstName: 'gaurav',
-	lastName: 'sharma',
+// const userDetail = {
+// 	firstName: 'gaurav',
+// 	lastName: 'sharma',
 	// fullName: function () {
 	// 	console.log('this', userDetail);
 	// 	return userDetail.firstName + ' ' + userDetail.lastName;
 	// },
 
-	fullName: () => {
-		console.log('this', this);
-		return userDetail.firstName + ' ' + userDetail.lastName;
-	},
-};
+// 	fullName: () => {
+// 		console.log('this', this);
+// 		return userDetail.firstName + ' ' + userDetail.lastName;
+// 	},
+// };
 
-console.log(userDetail.fullName());
+// console.log(userDetail.fullName());
+
+// ---------------------- all about doubt -----------------------------------
+
+// 27-feb-2024 – (Gaurav Sharma)
+// When passing number as arguments in function, It is treating as a string eg is below
+// function Rest_opt(...rest)
+// {
+//     return rest + rest;
+// }
+
+// var res = Rest_opt(23, 23, 32, 89, 89, 89);
+// console.log(res);
+
+// Result : 23, 23, 32, 89, 89, 8923, 23, 32, 89, 89, 89
+
+function Rest_opt(...rest) { // rest = [2, 3, 12, 5]
+	let result = 0;
+
+	for(let index=0; index<rest.length; index++) {
+		result += rest[index];
+	}
+
+	return result
+}
+
+var res = Rest_opt(4,3,1,6);
+console.log("result", res)
+
+// // Result: 23, 23, 32, 89, 89, 8923, 23, 32, 89, 89, 89;
+
+// let arr = [1,2,323,223,2]
+
+// arr[1]
+
